ad1095c3201e99c058e9d6b333d9ccc457a9b201
Guillaume Fournier <fournierg01@gmail.com>
2024-11-29 14:59:54 +0100

    destroy to watch_list

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "lists#index"
 
-
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
